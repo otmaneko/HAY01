ab4c410cf493b283f00240349cc740919363656e
otmaneko <64587662+otmaneko@users.noreply.github.com>
2020-05-15 21:17:38 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['668239213770309662']
+const myid = ['502820431200387072']
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
